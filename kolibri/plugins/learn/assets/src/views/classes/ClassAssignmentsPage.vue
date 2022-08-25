78d04620beb5befe1a6e78da473b29f2cdf7eed4
Kapya Sakala <kapsakala@gmail.com>
2022-08-25 09:42:32 +0200

    import mappers to get facility conifg. use function to return learner can view lessons value. use in view to conditionally display lessons for learner

--- kolibri/plugins/learn/assets/src/views/classes/ClassAssignmentsPage.vue
+++ kolibri/plugins/learn/assets/src/views/classes/ClassAssignmentsPage.vue
@@ -1,23 +1,15 @@
 <template>
-
   <div>
     <h1 dir="auto" class="classroom-name">
       {{ classroomName }}
     </h1>
-    <!-- <AssignedLessonsCards :lessons="lessons" :isMobile="windowIsSmall" />
- -->
-    <AssignedExamsCards
-      :exams="exams"
-      :isMobile="windowIsSmall"
-    />
+    <AssignedLessonsCards v-if="learnerCanViewLessons" :lessons="lessons" :isMobile="windowIsSmall" />
+    <AssignedExamsCards :exams="exams" :isMobile="windowIsSmall" />
   </div>
-
 </template>
-
-
 <script>
 
-  import { mapState } from 'vuex';
+  import { mapState, mapGetters } from 'vuex';
   import responsiveWindow from 'kolibri.coreVue.mixins.responsiveWindow';
   import AssignedExamsCards from './AssignedExamsCards';
   import AssignedLessonsCards from './AssignedLessonsCards';
@@ -35,11 +27,15 @@
     },
     mixins: [responsiveWindow],
     computed: {
+      ...mapGetters(['facilityConfig']),
       ...mapState('classAssignments', {
         classroomName: state => state.currentClassroom.name,
         exams: state => state.currentClassroom.assignments.exams,
         lessons: state => state.currentClassroom.assignments.lessons,
       }),
+      learnerCanViewLessons() {
+        return this.facilityConfig.learnerCanViewLessons;
+      },
     },
     $trs: {
       documentTitle: 'Class assignments',
@@ -47,12 +43,9 @@
   };
 
 </script>
-
-
 <style lang="scss" scoped>
-
-  .classroom-name {
-    margin-bottom: 32px;
-  }
+.classroom-name {
+  margin-bottom: 32px;
+}
 
 </style>
