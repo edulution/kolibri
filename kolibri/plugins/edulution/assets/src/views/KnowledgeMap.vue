ac8dfa555fc0621e370ba2aac88f4a9175db5af7
Kapya Sakala <kapsakala@gmail.com>
2022-09-01 16:37:30 +0200

    improved formatting

--- kolibri/plugins/edulution/assets/src/views/KnowledgeMap.vue
+++ kolibri/plugins/edulution/assets/src/views/KnowledgeMap.vue
@@ -4,91 +4,106 @@
       <h1 class="page-header">
         {{ topic.title }}
       </h1>
-      <TextTruncator v-if="topic.description" :text="topic.description" :maxHeight="90" :showTooltip="false" :showViewMore="true" dir="auto" class="page-description" />
+      <TextTruncator 
+        v-if="topic.description" 
+        :text="topic.description" 
+        :maxHeight="90" 
+        :showTooltip="false" 
+        :showViewMore="true" 
+        dir="auto" 
+        class="page-description"
+      />
     </div>
     <div v-if="contents[0].children.length">
       <ExpandableContentCardGroupGrid v-for="child in contents" :key="child.id" :child="child" />
     </div>
-    <ContentCardGroupGrid v-if="!contents[0].children.length" :contents="contents" :genContentLink="genContentLink" style="padding-left: 16px; padding-top: 16px" />
+    <ContentCardGroupGrid 
+      v-if="!contents[0].children.length" 
+      :contents="contents" 
+      :genContentLink="genContentLink" 
+      style="padding-left: 16px; 
+      padding-top: 16px"
+    />
     <PrerequisitesModal v-if="modalShown" />
   </div>
 </template>
 <script>
-import { mapGetters, mapState } from 'vuex';
-import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
-import TextTruncator from 'kolibri.coreVue.components.TextTruncator';
-import ProgressIcon from 'kolibri.coreVue.components.ProgressIcon';
-import { PageNames } from '../constants';
-import ContentCardGroupGrid from './ContentCardGroupGrid';
-import ExpandableContentCardGroupGrid from './ExpandableContentCardGroupGrid';
-import PrerequisitesModal from './PrerequisitesModal';
 
-export default {
-  name: 'KnowledgeMap',
-  $trs: {
-    topics: 'Topics',
-    navigate: 'Navigate content using headings',
-    documentTitleForChannel: 'Topics - { channelTitle }',
-    documentTitleForTopic: '{ topicTitle } - { channelTitle }',
-  },
-  components: {
-    ExpandableContentCardGroupGrid,
-    ContentCardGroupGrid,
-    TextTruncator,
-    ProgressIcon,
-    PrerequisitesModal,
-  },
-  metaInfo() {
-    let title;
-    if (this.isRoot) {
-      title = this.$tr('documentTitleForChannel', {
-        channelTitle: this.channelTitle,
-      });
-    } else {
-      title = this.$tr('documentTitleForTopic', {
-        channelTitle: this.channelTitle,
-        topicTitle: this.topic.title,
-      });
-    }
-    return { title };
-  },
-  data: function() {
-    return {
-      hidden: false,
-    };
-  },
-  computed: {
-    ...mapGetters(['isLearner']),
-    ...mapState('topicsTree', [
-      'channel',
-      'contents',
-      'isRoot',
-      'topic',
-      'progress',
-      'modalShown',
-    ]),
-    channelId() {
-      return this.channel.id;
+  import { mapGetters, mapState } from 'vuex';
+  import { ContentNodeKinds } from 'kolibri.coreVue.vuex.constants';
+  import TextTruncator from 'kolibri.coreVue.components.TextTruncator';
+  import ProgressIcon from 'kolibri.coreVue.components.ProgressIcon';
+  import { PageNames } from '../constants';
+  import ContentCardGroupGrid from './ContentCardGroupGrid';
+  import ExpandableContentCardGroupGrid from './ExpandableContentCardGroupGrid';
+  import PrerequisitesModal from './PrerequisitesModal';
+
+  export default {
+    name: 'KnowledgeMap',
+    $trs: {
+      topics: 'Topics',
+      navigate: 'Navigate content using headings',
+      documentTitleForChannel: 'Topics - { channelTitle }',
+      documentTitleForTopic: '{ topicTitle } - { channelTitle }',
     },
-    channelTitle() {
-      return this.channel.title;
+    components: {
+      ExpandableContentCardGroupGrid,
+      ContentCardGroupGrid,
+      TextTruncator,
+      ProgressIcon,
+      PrerequisitesModal,
     },
-  },
-  methods: {
-    genContentLink(id, kind) {
-      if (kind === ContentNodeKinds.TOPIC) {
-        return {
-          name: PageNames.KNOWLEDGE_MAP,
-          params: { channel_id: this.channelId, id },
-        };
+    metaInfo() {
+      let title;
+      if (this.isRoot) {
+        title = this.$tr('documentTitleForChannel', {
+          channelTitle: this.channelTitle,
+        });
+      } else {
+        title = this.$tr('documentTitleForTopic', {
+          channelTitle: this.channelTitle,
+          topicTitle: this.topic.title,
+        });
       }
+      return { title };
+    },
+    data: function() {
       return {
-        name: PageNames.TOPICS_CONTENT,
-        params: { channel_id: this.channelId, id },
+        hidden: false,
       };
     },
-  },
-};
+    computed: {
+      ...mapGetters(['isLearner']),
+      ...mapState('topicsTree', [
+        'channel',
+        'contents',
+        'isRoot',
+        'topic',
+        'progress',
+        'modalShown',
+      ]),
+      channelId() {
+        return this.channel.id;
+      },
+      channelTitle() {
+        return this.channel.title;
+      },
+    },
+    methods: {
+      genContentLink(id, kind) {
+        if (kind === ContentNodeKinds.TOPIC) {
+          return {
+            name: PageNames.KNOWLEDGE_MAP,
+            params: { channel_id: this.channelId, id },
+          };
+        }
+        return {
+          name: PageNames.TOPICS_CONTENT,
+          params: { channel_id: this.channelId, id },
+        };
+      },
+    },
+  };
 
 </script>
 <style lang="scss" scoped>
