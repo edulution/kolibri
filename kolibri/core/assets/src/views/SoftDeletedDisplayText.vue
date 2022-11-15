6c79ba1db6e38e382555ff0e7c4eaa44ff7caaec
Kapya Sakala <kapsakala@gmail.com>
2022-11-15 13:10:14 +0200

    added component for soft delete display text

---
+++ kolibri/core/assets/src/views/SoftDeletedDisplayText.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>{{ displayText }}</span>
+</template>
+<script>
+  import commonCoreStrings from 'kolibri.coreVue.mixins.commonCoreStrings';
+  import { FacilityUserSoftDeleted } from 'kolibri.coreVue.vuex.constants';
+  const { TRUE, FALSE } = FacilityUserSoftDeleted;
+  export default {
+    name: 'SoftDeletedDisplayText',
+    mixins: [commonCoreStrings],
+    props: {
+      deleted: {
+        type: Boolean,
+      },
+    },
+    computed: {
+      displayText() {
+        if (this.deleted === TRUE) {
+          return this.coreString('deletedOptionDeactivated');
+        } else if (this.deleted === FALSE) {
+          return this.coreString('deletedOptionActive');
+        }
+        return null;
+      },
+    },
+  };
+</script>
+<style lang="scss" scoped></style> 
