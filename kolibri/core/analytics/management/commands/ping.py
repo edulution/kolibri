8143c61da177e7b97ea52b2b5c6ed474e1e66ca4
Kapya Sakala <kapsakala@gmail.com>
2024-02-04 20:05:14 +0200

    disable ping by commenting out most of handle function

--- kolibri/core/analytics/management/commands/ping.py
+++ kolibri/core/analytics/management/commands/ping.py
@@ -38,17 +38,32 @@ class Command(BaseCommand):
     help = "Pings a central server to check for updates/messages and track stats."
 
     def add_arguments(self, parser):
-        parser.add_argument('--interval', action='store', dest='interval',
-                            help='Number of minutes to wait after a successful ping before the next ping.')
-        parser.add_argument('--checkrate', action='store', dest='checkrate',
-                            help='Number of minutes to wait between failed ping attempts.')
-        parser.add_argument('--server', action='store', dest='server',
-                            help='Base URL of the server to connect to.')
-        parser.add_argument('--once', action='store_true', dest='once',
-                            help='Only try to ping once, then exit')
+        parser.add_argument(
+            "--interval",
+            action="store",
+            dest="interval",
+            help="Number of minutes to wait after a successful ping before the next ping.",
+        )
+        parser.add_argument(
+            "--checkrate",
+            action="store",
+            dest="checkrate",
+            help="Number of minutes to wait between failed ping attempts.",
+        )
+        parser.add_argument(
+            "--server",
+            action="store",
+            dest="server",
+            help="Base URL of the server to connect to.",
+        )
+        parser.add_argument(
+            "--once",
+            action="store_true",
+            dest="once",
+            help="Only try to ping once, then exit",
+        )
 
     def handle(self, *args, **options):
-
         interval = float(options.get("interval") or DEFAULT_PING_INTERVAL)
         checkrate = float(options.get("checkrate") or DEFAULT_PING_CHECKRATE)
         server = options.get("server") or DEFAULT_SERVER_URL
@@ -56,34 +71,34 @@ class Command(BaseCommand):
 
         self.started = datetime.now()
 
-        while True:
-            try:
-                logger.info("Attempting a ping.")
-                with vacuum_db_lock:
-                    data = self.perform_ping(server)
-                    logger.info("Ping succeeded! (response: {})".format(data))
-                    create_and_update_notifications(data, nutrition_endpoints.PINGBACK)
-                    if "id" in data:
-                        stat_data = self.perform_statistics(server, data["id"])
-                        create_and_update_notifications(stat_data, nutrition_endpoints.STATISTICS)
-                    connection.close()
-                if once:
-                    break
-                logger.info("Sleeping for {} minutes.".format(interval))
-                time.sleep(interval * 60)
-                continue
-            except ConnectionError:
-                logger.warn("Ping failed (could not connect). Trying again in {} minutes.".format(checkrate))
-            except Timeout:
-                logger.warn("Ping failed (connection timed out). Trying again in {} minutes.".format(checkrate))
-            except RequestException as e:
-                logger.warn("Ping failed ({})! Trying again in {} minutes.".format(e, checkrate))
-            if once:
-                break
-            time.sleep(checkrate * 60)
+        # while True:
+        #     try:
+        #         logger.info("Attempting a ping.")
+        #         with vacuum_db_lock:
+        #             data = self.perform_ping(server)
+        #             logger.info("Ping succeeded! (response: {})".format(data))
+        #             create_and_update_notifications(data, nutrition_endpoints.PINGBACK)
+        #             if "id" in data:
+        #                 stat_data = self.perform_statistics(server, data["id"])
+        #                 create_and_update_notifications(stat_data, nutrition_endpoints.STATISTICS)
+        #             connection.close()
+        #         if once:
+        #             break
+        #         logger.info("Sleeping for {} minutes.".format(interval))
+        #         time.sleep(interval * 60)
+        #         continue
+        #     except ConnectionError:
+        #         logger.warn("Ping failed (could not connect). Trying again in {} minutes.".format(checkrate))
+        #     except Timeout:
+        #         logger.warn("Ping failed (connection timed out). Trying again in {} minutes.".format(checkrate))
+        #     except RequestException as e:
+        #         logger.warn("Ping failed ({})! Trying again in {} minutes.".format(e, checkrate))
+        #     if once:
+        #         break
+        #     time.sleep(checkrate * 60)
+        pass
 
     def perform_ping(self, server):
-
         url = urljoin(server, "/api/v1/pingback")
 
         instance, _ = InstanceIDModel.get_or_create_current_instance()
@@ -123,10 +138,11 @@ class Command(BaseCommand):
         return json.loads(response.content.decode() or "{}")
 
     def perform_statistics(self, server, pingback_id):
-
         url = urljoin(server, "/api/v1/statistics")
 
-        channels = [extract_channel_statistics(c) for c in ChannelMetadata.objects.all()]
+        channels = [
+            extract_channel_statistics(c) for c in ChannelMetadata.objects.all()
+        ]
         facilities = [extract_facility_statistics(f) for f in Facility.objects.all()]
 
         data = {
