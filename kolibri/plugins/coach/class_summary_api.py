96eaff77eff552af499da096e534b5d146a03d6b
akashdraut <39619563+akashdraut@users.noreply.github.com>
2024-03-27 17:06:09 +0530

    UPDATE: Added assessment_learner_status key in class_summary API

--- kolibri/plugins/coach/class_summary_api.py
+++ kolibri/plugins/coach/class_summary_api.py
@@ -81,6 +81,48 @@ def _get_quiz_status(queryset):
     return items
 
 
+def _get_assessment_status(queryset):
+    queryset = queryset.filter(
+        mastery_level__lt=0,
+    ).order_by("-end_timestamp")
+    queryset = queryset.annotate(
+        previous_masterylog=Subquery(
+            queryset.filter(
+                summarylog=OuterRef("summarylog"),
+                end_timestamp__lt=OuterRef("end_timestamp"),
+            ).values_list("id")[:1]
+        ),
+    )
+    queryset = annotate_response_summary(queryset)
+    items = []
+    statuses = queryset.annotate(
+        last_activity=Max("attemptlogs__end_timestamp"),
+        previous_num_correct=SQCount(
+            logger_models.AttemptLog.objects.filter(
+                masterylog=OuterRef("previous_masterylog"), correct=1
+            )
+            .order_by()
+            .values_list("item")
+            .distinct(),
+            field="item",
+        ),
+    ).values(
+        "summarylog__content_id",
+        "complete",
+        "last_activity",
+        "num_correct",
+        "num_answered",
+        "previous_num_correct",
+        learner_id=F("user_id"),
+    )
+    seen = set()
+    for item in statuses:
+        key = "{}-{}".format(item["learner_id"], item["summarylog__content_id"])
+        if key not in seen:
+            items.append(item)
+            seen.add(key)
+    return items
+
 def content_status_serializer(lesson_data, learners_data, classroom):  # noqa C901
 
     # First generate a unique set of content node ids from all the lessons
@@ -227,11 +269,25 @@ def _map_exam_status(item):
     return item
 
 
+def _map_assessment_status(item):
+    complete = item.pop("complete")
+    item["status"] = COMPLETED if complete else STARTED
+    item["assessment_id"] = item.pop("summarylog__content_id")
+    return item
+
+
 def serialize_coach_assigned_quiz_status(queryset):
     queryset = logger_models.MasteryLog.objects.filter(
         summarylog__content_id__in=queryset.values("id"),
     ).order_by()
     return list(map(_map_exam_status, _get_quiz_status(queryset)))
+
+
+def serialize_coach_assigned_assessment_status(queryset):
+    queryset = logger_models.MasteryLog.objects.filter(
+        summarylog__content_id__in=queryset.values("id"),
+    ).order_by()
+    return list(map(_map_assessment_status, _get_assessment_status(queryset)))
 
 
 def serialize_groups(queryset):
@@ -495,6 +551,7 @@ class ClassSummaryViewSet(viewsets.ViewSet):
             "exams": exam_data,
             "assessments": assessment_data,
             "exam_learner_status": serialize_coach_assigned_quiz_status(query_exams),
+            "assessment_learner_status": serialize_coach_assigned_assessment_status(query_assessments),
             "content": content,
             "content_learner_status": content_status_serializer(
                 lesson_data, learners_data, classroom
