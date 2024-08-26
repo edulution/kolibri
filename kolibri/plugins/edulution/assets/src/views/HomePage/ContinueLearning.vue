f025c4866757e2b8fa4e07f9b1429e0225b6129c
Sword <ntch98@gmail.com>
2024-08-26 11:45:39 +0200

    💅 Style changes - Renamed to  "Continue learning on your own"

--- kolibri/plugins/edulution/assets/src/views/HomePage/ContinueLearning.vue
+++ kolibri/plugins/edulution/assets/src/views/HomePage/ContinueLearning.vue
@@ -162,12 +162,12 @@
     },
     $trs: {
       continueLearningOnYourOwnHeader: {
-        message: 'Continue learning on your own',
+        message: 'Continue where you left off',
         context:
           'Option to continue interacting with the resources (lessons, quizzes) in a self-directed way or through free exploration, rather than via material that coaches have prepared and made available in classes.',
       },
       continueLearningFromClassesHeader: {
-        message: 'Continue learning from your classes',
+        message: 'Continue where you left off',
         context:
           'Option to continue interacting with the resources (lessons, quizzes) coaches have prepared and made available in the classes learner is enrolled into.',
       },
