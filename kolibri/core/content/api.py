1fe4609600594ff31ee1bebbb9a2fb0d2a536cec
Kapya <kapsakala@gmail.com>
2024-12-25 12:55:25 +0200

    re-added exception to get_children function

--- kolibri/core/content/api.py
+++ kolibri/core/content/api.py
@@ -1854,6 +1854,10 @@ class KnowledgeMapViewset(ReadOnlyModelViewSet):
 
         def get_children(parent_id):
             children = ContentNode.objects.filter(parent=parent_id, available=True)
+
+            if not children:
+                raise Exception('No Content Available For ID :',parent_id )
+            
             serialized = ContentNodeSlimSerializer(children, many=True).data
             for c, s in zip(children, serialized):
                 s['progress_fraction'] = get_progress(c)
